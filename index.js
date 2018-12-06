2f8079fa6d32eebff758840c99e9a077f3b0bd13
debck <debasish.sahoo1998@gmail.com>
2018-12-06 20:23:25 +0530

    added babel and fixed errors :tada:

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
- var showdown = require("showdown");
+const express = require('express');
+const app = express();
+const showdown = require("showdown");
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -11,7 +11,7 @@ app.get('/', (req,res) => {
 	res.render('home');
 });
 
-var port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () =>{
-	console.log("Server started.....");
+	console.log(`Server started at ${port}`);
 });
