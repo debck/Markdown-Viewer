1d8737aa94d33f83d7bf660d5be34d2d2449ce6e
Wolfgang Kreminger <w.kreminger@deep-nature.at>
2019-10-18 14:58:09 +0200

    adding copy text button, adding functionality

--- public/script.js
+++ public/script.js
@@ -1,17 +1,32 @@
-
 window.onload = function() {
-    var showdown = require("showdown");
-    var converter = new showdown.Converter();
-    var input = document.getElementById('input');
-    var markdownArea = document.getElementById('markdown');   
+  var showdown = require("showdown");
+  var converter = new showdown.Converter();
+  var input = document.getElementById("input");
+  var markdownArea = document.getElementById("markdown");
+  const copy = document.querySelector("#copy");
 
-    var convertTextAreaToMarkdown = function(){
-        var markdownText = input.value;
-        html = converter.makeHtml(markdownText);
-        markdownArea.innerHTML = html;
-    };
+  var convertTextAreaToMarkdown = function() {
+    var markdownText = input.value;
+    html = converter.makeHtml(markdownText);
+    markdownArea.innerHTML = html;
+  };
 
-    input.addEventListener('input', convertTextAreaToMarkdown);
+  input.addEventListener("input", convertTextAreaToMarkdown);
 
-    convertTextAreaToMarkdown();
+  convertTextAreaToMarkdown();
+
+  const copyToClipboard = () => {
+    const el = document.createElement("textarea");
+    const str = input.value;
+    el.value = str;
+    el.setAttribute("readonly", "");
+    el.style.position = "absolute";
+    el.style.left = "-9999px";
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+  };
+
+  copy.addEventListener("click", copyToClipboard);
 };
